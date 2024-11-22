2177ebda95094f87c96fc881f03f279b0941122e
Willy Ellwart <willellwart@gmail.com>
2024-11-22 09:22:53 +0100

    nochmal2

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-      bucket = "github-actions-tfstate-bucket"
+      bucket = "myawesomebucket7"
       key = "github-actions.tfstate"
       region = "eu-central-1"
     }
